5dfb739d0ebfbaf83dcb4da1230477999ee39295
Yin <zyta6rift@gmail.com>
2025-03-19 19:27:36 +0800

    feat(ts): 利用陣列紀錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Product } from "./product";
 
-let product = new Product(1,"圖像 Angular 開發入門 第二版","博碩文化",500,true,new Date ("2024-4-10"));
+let product = [
+    new Product(1,"圖像 Angular 開發入門 第二版","博碩文化",500,true,new Date ("2024-4-10")),
+    new Product(2,"金魚都能懂的 CSS 必學屬性","博碩文化",500,true,new Date ("2024-4-10"))
+];
 
 console.log(product);
 console.table(product);
