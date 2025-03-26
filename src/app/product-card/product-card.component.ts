d16e7f99825adee41217d9133bc80f2370f3ebd6
Yin <lyoun.yen131@gmail.com>
2025-03-26 18:20:54 +0800

    feat(product): 利用文字插值顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  productName = '書籍 A';
+  author = '作者甲、作者乙、作者丙';
+  company = '博碩文化';
 }
