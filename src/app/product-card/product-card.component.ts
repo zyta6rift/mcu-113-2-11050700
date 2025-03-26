fb1983247f1096447219695a8df63a16e88a8a79
Yin <lyoun.yen131@gmail.com>
2025-03-26 17:48:49 +0800

    feat(product): 產品卡片樣式實作

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+
+}
